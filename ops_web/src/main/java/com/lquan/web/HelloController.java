5c9414e23363b694bb60c8e200d264e4fa835990
1570599338@qq.com <1570599338@qq.com>
2019-04-07 18:50:28 +0800

    test

--- ops_web/src/main/java/com/lquan/web/HelloController.java
+++ ops_web/src/main/java/com/lquan/web/HelloController.java
@@ -1,7 +1,13 @@
 package com.lquan.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lquan.ops.dao.SysUserMapper;
+import com.lquan.ops.model.po.SysUser;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * ClassName HelloController
@@ -14,11 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 
 @RestController
+@Slf4j
 public class HelloController {
+    @Autowired
+    private SysUserMapper service;
 
     @RequestMapping("/hello")
     public  String index(){
 
         return "hello world !";
+    }
+    
+    @RequestMapping("/hello2")
+    public  String index2(){
+    	SysUser user = service.selectByPrimaryKey(2);
+
+        return user.getNickname();
     }
 }
