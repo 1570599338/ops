d169532d44e4c166675da3e30a4897bb78ae0dee
1570599338@qq.com <1570599338@qq.com>
2019-04-07 17:55:56 +0800

    mysql

---
+++ ops_service/src/main/java/com/lquan/ops/dao/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.lquan.ops.dao;
+
+import com.lquan.ops.model.po.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
